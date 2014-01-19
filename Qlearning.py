83bad0c5746a9b0102b80b7ff160ba656d6217b8
Elliot Colp <thelastbanana@gmail.com>
2014-01-19 16:40:53 -0700

    Added gamma.

--- Qlearning.py
+++ Qlearning.py
@@ -6,6 +6,7 @@ class Qlearning(Agent):
         self.Q = 0.00001 * rand(STATE_COUNT, ACTION_COUNT)
         self.epsilon = 0.5
         self.alpha = 0.01
+        self.gamma = 1
     
     def do_step(self, S, act):
         Agent.do_step(self, S, act)
@@ -24,5 +25,5 @@ class Qlearning(Agent):
         # max_a(Q(S', a))
         nextmax = 0 if Sp == TILE_GOAL else max(self.Q[Sp])
         
-        # Update Q for this state/action pair (assume gamma = 1)
-        self.Q[S][A] += self.alpha * (R + nextmax - self.Q[S][A])
+        # Update Q for this state/action pair
+        self.Q[S][A] += self.alpha * (R + self.gamma * nextmax - self.Q[S][A])
